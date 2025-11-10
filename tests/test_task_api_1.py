461e73b0e427bd621505d539d04853f1d3fcc795
Nikolai Mitroshkin <ex3ct@mail.ru>
2025-11-10 19:31:24 +0300

    added api task 1

---
+++ tests/test_task_api_1.py
@@ -0,0 +1,26 @@
+import requests
+
+
+# Урок 6.4
+"""Класс, в котором получаем одну рандомную шутку по каждой категории"""
+class TestOneJokeFromEachCategory:
+    url_categories = "https://api.chucknorris.io/jokes/categories"
+
+    """Проверка получения рандомной шутки по каждой категории"""
+    def test_get_one_joke_from_each_category(self):
+        """Запрос всех категорий"""
+        all_categories = requests.get(self.url_categories)
+
+        """Запрос по одной шутке из каждой категории"""
+        for category in all_categories.json():
+            result = requests.get(f"https://api.chucknorris.io/jokes/random?category={category}")
+
+            print(f"Статус код: {result.status_code}")
+            assert result.status_code == 200, "Неудача! Запрос вернул ошибку."
+            print(f"Ваша шутка: {result.json()}")
+            print(f"Успех! Мы получили рандомную шутку по категории - {category}.")
+            print("Следующая шутка")
+
+
+randon_joke_in_category = TestOneJokeFromEachCategory()
+randon_joke_in_category.test_get_one_joke_from_each_category()
